028dc8b1df474977e41bee786728bf5c43436f29
windy664 <2580855214@qq.com>
2025-01-18 08:46:27 +0800

    注册neoforge监听器！

--- src/main/java/org/windy/guildshelter/Listener/PlayerInteractListener.java
+++ src/main/java/org/windy/guildshelter/Listener/PlayerInteractListener.java
@@ -12,6 +12,23 @@ public class PlayerInteractListener extends JavaPlugin implements Listener {
     private static final Logger LOGGER = LogManager.getLogger();
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent event) {
+
         LOGGER.info("PlayerInteractEvent 触发");
+
+        String deplayer = event.getEntity().getName().toString();
+        // 检查实体的名字是否包含特定的子字符串
+        if (deplayer.contains("AS-FAKEPLAYER") ||
+                deplayer.contains("[MINECRAFT]") ||
+                deplayer.contains("[MEKANISM]") ||
+                deplayer.contains("[IF]") ||
+                deplayer.contains("[IntegratedTunnels]") ||
+                deplayer.contains("KILLER JOE") ||
+                deplayer.contains("[DEPOLYER]") ||
+                deplayer.contains("[XU2FAKEPLAYER]") ||
+                deplayer.contains("[MODULAR ROUTERS]")) {
+            // 如果玩家名字匹配条件，返回，不处理事件
+            LOGGER.info("假玩家" + deplayer + " 触发了 PlayerInteractEvent");
+            return;
+        }
     }
 }
